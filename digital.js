599ebe4cf5f4b364cff09fca23a94dee813d8e39
mhlehri <lehrimirza101@gmail.com>
2023-08-14 02:49:31 +0600

    digital tajbih

---
+++ digital.js
@@ -0,0 +1,33 @@
+const jikir1 = document.getElementById("h1");
+const jikir2 = document.getElementById("h2");
+const jikir3 = document.getElementById("h3");
+count1 = 0;
+function make1() {
+  if (count1 < 34) {
+    count1++;
+  }
+  document.getElementById("h1").innerText = count1;
+}
+
+count2 = 0;
+function make2() {
+  count2++;
+  if (count2 < 34) {
+    count2++;
+  }
+  document.getElementById("h2").innerText = count2;
+}
+count3 = 0;
+function make3() {
+  count3++;
+  if (count3 < 34) {
+    count3++;
+  }
+  document.getElementById("h3").innerText = count3;
+}
+document.getElementById("btn").addEventListener("click", function () {
+  empty = "";
+  jikir1.innerText = empty;
+  jikir2.innerText = empty;
+  jikir3.innerText = empty;
+});
